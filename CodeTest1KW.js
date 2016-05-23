7349e2a35bdc798d545c06869c6e2d2fb74d01ad
Ke Wang <Ke Wang>
2016-05-23 09:35:35 -0700

    first commit

---
+++ CodeTest1KW.js
@@ -0,0 +1,14 @@
+function printCameronSean() {
+    for (var i = 1; i <= 100; i++) {
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log('CameronSean');
+        } else if (i % 5 == 0) {
+            console.log('Sean');
+        } else if (i % 3 == 0) {
+            console.log('Cameron');
+        } else {
+            console.log(i);
+        }
+    }
+}
